5b9493acbd7255c0fae10f760eb93c390bc6eb2d
somros <alberto.rovellini@gmail.com>
2016-05-05 16:23:14 +1200

    Finished and functioning script. Size spectrum routine modified to dynamicslly adapt the weight bin to the maximum size recorded for the individuals for each given functinal group across all the replicates. Plot including ALL the separated size spectra for each functional group. Shape of the spectra is vaguely dome-shaped with lumps and trenches. Need to validate it against existing literature.

--- groupSpectra.R
+++ groupSpectra.R
@@ -1,25 +1,29 @@
 # script for size spectra disaggregated on the functional groups
 # 29.04.2016. Alberto Rovellini, Victoria University of Wellington
 
-
 require(abind)
 require(plyr)
+require(ggplot2)
 setwd("/home/somros/Documents/itn100results/sizeSpectrum2000/demo")
-list<-list.files("/home/somros/Documents/itn100results/sizeSpectrum2000/demo", 
-                recursive=TRUE, pattern="*.csv") 
+list<-list.files("/home/somros/Documents/itn100results/sizeSpectrum2000/demo",
+                recursive=TRUE, pattern="*.csv")
 length.list<-length(list)
 read.special<-function(x) {
  read.table(x, header=TRUE, sep='\t', dec='.') # custom function to read the batches of .csv keeping the header
 }
 data_list <- lapply(list, read.special) # stores all the files into a huge list
+funGroupsNames <- c("smallpelagic", "mediumpelagic", "largepelagic", "smalldemersal",
+                    "mediumdemersal", "largedemersal", "topcarnivores")
 
-functionalGroupSeparator <- function(dataFrame) {
+
+functionalGroupSeparator <- function(dataFrame) { # for some reason some go in alphabetical
   dataFrame$class. <- as.character(dataFrame$class.)
-  dataFrame$class. <- factor(dataFrame$class., levels = unique(dataFrame$class.))
+  dataFrame$class. <- factor(dataFrame$class., levels = funGroupsNames)
   splitFrames <- split(dataFrame, dataFrame$class.)
   return(splitFrames)
 }
- 
+
+
 listOfDisaggregatedLists <- lapply(data_list, functionalGroupSeparator)
 
 test <- functionalGroupSeparator(data_list[[1]])
@@ -44,19 +48,28 @@ for (j in 1:classes) {
   }
 }
 
-# now spectra routines must be applied to averages
-# same function applied to tot ind must be applied to each frame recursively. double lapply, likely.
-# need to find solution for size bins, because they must change for different classes. could use
-# max size/100 and see what happens.
+# function to detect the highest biomass for each functional group across all the replicates
 
-# need to run a function over the replicates to identify the highest biomass and use that as safety bin
-# for each functional group
+highestBiomass <- function(listOfReplicates) {
+  listMaxB <- vector(mode="list", length=length(listOfReplicates))
+  for (i in 1:length(listOfReplicates)) {
+    listMaxB[[i]] <- max(listOfReplicates[[i]]$biomass) 
+  }
+  vectorMaxB <- unlist(listMaxB)
+  maxBFunGroup <- max(vectorMaxB)
+  return(maxBFunGroup)
+}
+
+maxBins <- data.frame(funGroupsNames, unlist(lapply(largeList, highestBiomass)))
+colnames(maxBins) <- c("funGroup", "maxBiomass")
+
+# modify spectra calculator to dynamically apply the correct bin to the correct class
 
 frequencies <-function(data) { # function to extract the frequencies for each replicate
   mass <- data$biomass # isolates the column with biomass. infact, no need to factorize if the spectrum is for the whole
-  # community, which still has to be defined anyway
-  largestBin <- ceiling(max(mass))#+1000 # trick to have all the bins of the same length
-  breaks <- seq(0, largestBin, largestBin/100) # sets the breaks ranging over the biomass of the individuals
+  functionalGroup <- as.character(levels(factor(data$class))) # need to drop unused levels
+  largestBin <- ceiling(maxBins[maxBins$funGroup==functionalGroup,2])#+1000 # trick to have all the bins of the same length
+  breaks <- seq(0, largestBin, largestBin/50) # sets the breaks ranging over the biomass of the individuals
   length_classes <- c(1, breaks[2:(length(breaks)-1)]) # gets rid of the 0 and of the last value
   cat <- cut(mass, breaks, labels=length_classes, include.lowest=TRUE) # assigns the biomasses to their bin
   freq <- table(cat)
@@ -79,19 +92,58 @@ disaggregatedFreqList <- lapply(largeList, function(subList) lapply(subList, fre
 
 listOfMatrices <- lapply(disaggregatedFreqList, function(subList) {
   rawMatrix <- abind(subList, along=2)
-  cleanMatrix <- data.frame(rawMatrix[,1], rawMatrix[,seq(1, ncol(rawMatrix), by = 2)])
+  repMatrix <- data.frame(rawMatrix[,seq(2, ncol(rawMatrix), by = 2)])
+  repMatrix[is.na(repMatrix)] <- 0
+  meanFrequency <- apply(repMatrix, 1, mean)
+  sdFrequency <- apply(repMatrix, 1, sd)
+  cleanMatrix <- data.frame(rawMatrix[,1], meanFrequency, sdFrequency)
+  cleanMatrix[cleanMatrix==0] <- NA
+  colnames(cleanMatrix) <- c("logWeight", "logAbundance", "sdAbundance")
   return(cleanMatrix)
-  })
+})
 
+# at this point: a list of 7 dataframes, each about the abundance per weight bin of a given functional group
+# write a ggplot function and apply to it (output is going to suck). facet if possible.
 
-all.matrix <- abind(refined, along=3)
-all.matrix[is.na(all.matrix)] <- 0 # gets rid of NAs, check if legit lol
-mean_runs <- apply(all.matrix, c(1,2), mean) # gotcha
-sd_runs <- apply(all.matrix, c(1,2), sd) # gotcha
+plotFNSpectrum <- ggplot(data=listOfMatrices[[2]], aes(x=logWeight, y=logAbundance))+
+  geom_point()
+plotFNSpectrum
 
+# as alternative, put them all together in one frame and melt them and use one graph to capture them all
 
+for (i in 1:length(listOfMatrices)) {
+  listOfMatrices[[i]]$funGroup <- rep(funGroupsNames[i], nrow(listOfMatrices[[i]]))
+}
 
+continuousSpectrum <- as.data.frame(abind(listOfMatrices, along=1))
 
+# fix bloody classes of columns
 
+continuousSpectrum$logWeight <- as.numeric(levels(continuousSpectrum$logWeight))[continuousSpectrum$logWeight]
+continuousSpectrum$logAbundance <-as.numeric(levels(continuousSpectrum$logAbundance))[continuousSpectrum$logAbundance]
+continuousSpectrum$sdAbundance <- as.numeric(levels(continuousSpectrum$sdAbundance))[continuousSpectrum$sdAbundance]
+continuousSpectrum$funGroup <- factor(continuousSpectrum$funGroup, levels = unique(continuousSpectrum$funGroup))
 
+plotFNSpectrum <- ggplot(data=continuousSpectrum[!is.na(continuousSpectrum$logAbundance),], 
+                         aes(x=logWeight, y=logAbundance, group=funGroup))+
+  #geom_point(aes(shape=funGroup))+
+  geom_line(aes(colour=funGroup))+
+  scale_x_continuous(name="log(weight bin [100g])", 
+                     limits=c(3,10),
+                     breaks=seq(3,10,.5))+
+  scale_y_continuous(name="log(number of individuals)", 
+                     limits=c(0,10),
+                     breaks=seq(0,10,.5))+
+  scale_colour_manual(values = c("#377EB8", "#E41A1C", "#4DAF4A","#FF7F00","#984EA3","#999999","#F781BF"))+
+  theme(panel.background = element_rect(fill = 'white'))+
+  #theme
+  theme_bw()+
+  theme(panel.grid.minor = element_blank(), 
+        panel.grid.major = element_blank())+
+  theme(plot.title = element_text(size=14, vjust=2))+
+  theme(axis.title.x = element_text(size=10,vjust=-0.5),
+        axis.title.y = element_text(size=10,vjust=0.5))+
+  theme(axis.text.x=element_text(size=10))+
+  theme(axis.text.y=element_text(size=10))
+plotFNSpectrum
 
